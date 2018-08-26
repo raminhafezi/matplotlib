cc2239b5c060f00c9b18a67fbb73fcd1477dfeb9
raminhafezi@gmail.com <raminhafezi@gmail.com>
2018-08-27 00:10:45 +1000

    push

--- video_06.sh
+++ video_06.sh
@@ -5,28 +5,24 @@ import matplotlib.pyplot as plt
 	We are talking about Pie_Chart
 	startangel = 90 -> means to rotate the chart in clockwise
 	explode = (0, 0.1, 0, 0) is a way to gain focus on the second pie section which is Eating at this pie chart
-	autopct is write the percentage of each section and the %1.1f%% is a code to write percentage.
+	autopct is write the percentage of each section and the %1.1f%% is a code to write percentage. for example 29.2 becomes 29.2%.
+	The usage of the pie chart is that matplotlib calculate the percentage and the size of each secction in the pie chart automatically.
 	 
 '''
-days = ["Saturday", "Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday"]
-sleeping = [10, 9, 7, 8, 9, 8, 9 ]
-eating = [4, 4, 3, 2, 1, 2, 4]
-playing = [8, 10, 4, 1, 6, 5, 3]
-working = [2, 1, 10, 13, 8, 9, 8]
-
-clices = [7,2,2,13]
-Colors = ["cyan", "blue", "grey", "green"]
+slices = [7,2,5,9]
 Labels = ["Sleeping", "Eating", "Playing", "Working"]
 
+Colors = ["cyan", "blue", "grey", "green"]
+
 plt.pie(
-		clices, 
+		slices, 
 		colors = Colors , 
 		labels = Labels, 
-		startangle = 45, 
-		explode = (0, 0.1, 0, 0),
+		startangle = 90, 
+		explode = (0, 0.04, 0, 0),
+		shadow = True,
 		autopct = "%1.1f%%" )
 
-plt.title('Pie chart\n Check it out')
-
+plt.title('Pie chart')
 plt.legend()
 plt.show()
